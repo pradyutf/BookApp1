7f00593d626134d162e35566e759e5534150448a
pradyutf <pradyutfogla@gmail.com>
2024-01-18 01:41:54 +0530

    Initial Commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Pradyut Fogla on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookApp/BookdescView.swift
@@ -0,0 +1,18 @@
+//
+//  BookdescView.swift
+//  BookApp
+//
+//  Created by Pradyut Fogla on 16/01/24.
+//
+
+import SwiftUI
+
+struct BookdescView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookdescView()
+}
